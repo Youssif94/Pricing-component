e72cf18c7ee8c23680b1951c4e8808478d0c1051
Youssif94 <86874043+Youssif94@users.noreply.github.com>
2022-04-10 23:38:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const slider = document.querySelector('.slider')
 const pricesYear = document.querySelectorAll('.price_year')
 const pricesMonth = document.querySelectorAll('.price_month')
+const annually = document.querySelector('.annually')
 
 slider.addEventListener('click',function(e){
 pricesYear.forEach(span=> span.classList.toggle('hide'))
 pricesMonth.forEach(span=> span.classList.toggle('hide'))
+annually.classList.toggle('breathe');
+document.querySelector('.save_text').classList.toggle('hide')
 })
